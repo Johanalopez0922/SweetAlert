c328c02cd03fa5e93bb541631054bd51be4df27d
Johanalopez0922 <johasaron@gmail.com>
2023-10-17 15:12:14 -0500

    Inicializando Repo

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function showSweetAlert(){
+swal({
+    title: `¡Impresionante`,
+    text: "Modificaste un Alert!!",
+    imageUrl:
+      "https://t3.ftcdn.net/jpg/01/03/16/78/360_F_103167891_tygpYrCyJeN3sQTf3OuOD7uxDgHh4vVo.jpg",
+    imageSize: "100x100",
+    confirmButtonText: "Ok"
+  });
+}
+
