124ec7775e3e161400c723e09d1860801bf09ca8
Johanalopez0922 <johasaron@gmail.com>
2023-10-17 16:46:25 -0500

    HttpRequestGet

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 
 function showSweetAlert(){
 swal({
-    title: `¡Impresionante`,
+    title: `Impresionante!!`,
     text: "Modificaste un Alert!!",
     imageUrl:
       "https://t3.ftcdn.net/jpg/01/03/16/78/360_F_103167891_tygpYrCyJeN3sQTf3OuOD7uxDgHh4vVo.jpg",
-    imageSize: "100x100",
-    confirmButtonText: "Ok"
+    imageSize: "250x250",
+    confirmButtonText: "OK"
   });
 }
 
+
+const connectApi = (async) =>{
+    fetch("https://pokeapi.co/api/v2/pokemon/ditto",{
+        request: 'GET'
+    })
+    .then((res)=>{
+        res.json()
+        .then((data)=>{
+            console.log(data)
+        })
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+    .finally(()=>{
+        console.log("Option Finally!")
+    }); 
+}
